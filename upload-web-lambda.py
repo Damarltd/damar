84bbbae2911e4c8a7b1a197db7f3e1105e4d8018
Fredrik Wahlqvist <fredrik.wahlqvist@damar.ltd>
2018-10-12 00:19:16 +0100

    Added in function for code pipeline to be able to get bucketName and objectkey

--- upload-web-lambda.py
+++ upload-web-lambda.py
@@ -9,13 +9,24 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:255162520246:deployWebTopic')
 
+    location : {
+        "bucketName:": 'webbuild.damar.agency',
+        "objectKey": 'webbuild.zip'
+    }
     try:
+        job = event.get('CodePipeline.job')
 
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "WebPipeline":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building web from " + str(location)
         damaragency_bucket = s3.Bucket('damar.agency')
-        build_bucket = s3.Bucket('webbuild.damar.agency')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         webbuild_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('webbuild.zip', webbuild_zip)
+        build_bucket.download_fileobj(location["objectKey"], webbuild_zip)
 
         with zipfile.ZipFile(webbuild_zip) as myzip:
             for nm in myzip.namelist():
@@ -26,6 +37,9 @@ def lambda_handler(event, context):
 
         print "Job Done!"
         topic.publish(Subject="Website deployed", Message="Website build deployed succesfully!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
 
     except:
         topic.publish(Subject="Website NOT deployed", Message="Website build NOT deployed succesfully!")
