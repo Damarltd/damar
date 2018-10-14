3aff63263720193f2a11e72e99f3d1015766f5b2
Fredrik Wahlqvist <fredrik.wahlqvist@damar.ltd>
2018-10-14 11:42:45 +0100

    React js example for dynamic pages added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-import 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
-
-console.log("Webpack works");
+const myWork = [
+  {
+    'title': "Gather",
+    'image': {
+      'desc': "Data gathering image",
+      'src': "images/gather.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Analyse",
+    'image': {
+      'desc': "Data analysing image",
+      'src': "images/analyse.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Act",
+    'image': {
+      'desc': "Data adwords mcc image",
+      'src': "images/act.png",
+      'comment': ""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}  />, document.getElementById('example-work'))
